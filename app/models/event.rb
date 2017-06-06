418b26ec4d6abd92e40bb4f164b84db81f353969
stephaniehchen <stephaniechen@Stephanie-MacBook-Air.local>
2017-06-06 15:49:13 -0500

    update rb files

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,4 +8,6 @@ class Event < ApplicationRecord
   has_many :likes, :dependent => :destroy
   has_many :fans, :through => :likes, :source => :user
   belongs_to :organizer
+  mount_uploader :image_url, ImageUrlUploader
+
 end
